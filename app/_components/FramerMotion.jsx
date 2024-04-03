85594dc9cfd69f213a673cb3c9e62303a7b41425
assilbekov <askhat.assilbekov@gmail.com>
2024-04-03 11:33:29 -0300

    init app structure

---
+++ app/_components/FramerMotion.jsx
@@ -0,0 +1,9 @@
+
+
+export const FramerMotion = () => {
+  return (
+    <div>
+      <h1>My Framer Motion Page</h1>
+    </div>
+  )
+}
---
+++ app/_components/GSAP.jsx
@@ -0,0 +1,9 @@
+
+
+export const GSAP = () => {
+  return (
+    <div>
+      <h1>My GSAP Page</h1>
+    </div>
+  )
+}
