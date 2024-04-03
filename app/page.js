0efb05120b833a4cb2451eb103d1c32854dd9a3b
assilbekov <askhat.assilbekov@gmail.com>
2024-04-03 11:37:01 -0300

    init lenis

--- app/page.js
+++ app/page.js
@@ -1,10 +1,23 @@
 "use client"
 
+import Lenis from "@studio-freight/lenis";
 import styles from "./page.module.scss";
 import { FramerMotion } from "./_components/FramerMotion";
 import { GSAP } from "./_components/GSAP";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      window.requestAnimationFrame(raf);
+    }
+
+    window.requestAnimationFrame(raf);
+  }, [])
+
   return (
     <main className={styles.main}>
       <FramerMotion />
